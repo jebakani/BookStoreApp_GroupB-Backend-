77b17ff1558e38770ed56e133161b271fef34149
Jebakani <vjishwarya@gmail.com>
2021-09-27 10:53:49 +0530

    Initial commit

---
+++ BookStoreApp/Manager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
